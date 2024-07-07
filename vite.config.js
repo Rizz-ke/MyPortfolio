ff701fc118c5932915faeb51427c0e9b22c12fd5
Wafiq Mohamed <wafiqmohameddoo@gmail.com>
2024-07-08 02:01:19 +0300

    portfolio complteted

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: ['/assets/Logo.png'], // Adjust this path as per your project structure
-    },
+    outDir: 'build',
   },
 });
