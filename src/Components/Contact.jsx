44db4dd68054d890574535d963fc5d5122300b76
Wafiq Mohamed <wafiqmohameddoo@gmail.com>
2024-07-08 14:09:03 +0300

    portfolio complteted

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -77,7 +77,7 @@ const socialMediaLinks = [
     name: 'LinkedIn',
     link: 'https://www.linkedin.com/in/wafiq-mohamed-0b6465305/',
     icon: <FaLinkedin size={24} />,
-    image: '/src/assets/Linkedin.jpeg',
+    image: 'https://media.licdn.com/dms/image/D4D03AQFsSaBNmdR82A/profile-displayphoto-shrink_800_800/0/1713760144465?e=1726099200&v=beta&t=TkqOliUMq2RP9ZavAORv1IA9QPYbdgflIELW-xF9wfg',
   },
   {
     name: 'Twitter',
